866966dfb9ecb84da44d0037e450a269c27259b9
Marcos Duarte <pe.marcos30@gmail.com>
2024-07-25 11:18:41 -0300

    ajustes configuracoes

--- src/main/java/com/markdev/ws/mylist/MylistApplication.java
+++ src/main/java/com/markdev/ws/mylist/MylistApplication.java
@@ -2,10 +2,13 @@ package com.markdev.ws.mylist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
+@PropertySource({"classpath:application.properties", "classpath:db.properties"})
+
 public class MylistApplication {
 
 	public static void main(String[] args) {
